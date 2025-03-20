60fa6880d918f9cd28f819b65994b430a57db876
Ghaith955 <ghaithch955@gmail.com>
2025-03-20 20:57:15 +0100

    Update script.js

--- chmoula/script.js
+++ chmoula/script.js
@@ -50,3 +50,11 @@ if (mobileMenuButton && navMenu) {
         navMenu.classList.toggle('active');
     });
 }
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
